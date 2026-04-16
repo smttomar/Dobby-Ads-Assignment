c2f4f5783a1a33caf7e4638a96034df85620ec1f
smttomar <sumitsnghtmr@gmail.com>
2026-04-16 18:30:13 +0530

    Update Frontend API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: "https://dobby-ads-assignment-1pry.onrender.com/api",
 });
 
 // Attach token automatically
