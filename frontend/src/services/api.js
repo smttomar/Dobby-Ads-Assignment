179ff959e161a6970595936421cd7f5aec247f05
smttomar <sumitsnghtmr@gmail.com>
2026-04-16 19:20:38 +0530

    change upload image to cloudinary

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    // baseURL: "https://dobby-ads-assignment-1pry.onrender.com/api",
-    baseURL: "http://localhost:8000/api",
+    baseURL: "https://dobby-ads-assignment-1pry.onrender.com/api",
+    // baseURL: "http://localhost:8000/api",
 });
 
 // Attach token automatically
